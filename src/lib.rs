9abfe8087159d483ae5bd8c3c2f81f64d417b410
wired-maya <dhodul@gmail.com>
2024-08-27 17:08:04 -0700

    Added char focusing in FieldGridMap

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-use godot::{builtin::{Dictionary, GString, Plane, Rect2, StringName, Variant, Vector2, Vector2i, Vector3, Vector3i}, classes::{canvas_item, Camera3D, CanvasItem, ColorRect, GridMap, ICamera3D, IColorRect, IGridMap, INode, ISubViewport, InputEvent, InputEventMouse, InputEventMouseButton, InputEventMouseMotion, Material, Node, PhysicsDirectSpaceState3D, PhysicsRayQueryParameters3D, PhysicsServer3D, Shader, ShaderMaterial, SubViewport, Time, Viewport}, global::{godot_print, MouseButton}, init::{gdextension, ExtensionLibrary}, meta::FromGodot, obj::{Base, Gd, WithBaseField}, prelude::{godot_api, GodotClass, Var}};
+use std::collections::HashMap;
+
+use godot::{builtin::{Array, Dictionary, Plane, Rect2, StringName, Variant, Vector2, Vector2i, Vector3, Vector3i}, classes::{Camera3D, CanvasItem, CharacterBody3D, ColorRect, Engine, GridMap, ICamera3D, ICharacterBody3D, IColorRect, IGridMap, INode, ISubViewport, InputEvent, InputEventMouse, InputEventMouseButton, InputEventMouseMotion, Material, Node, PhysicsDirectSpaceState3D, PhysicsRayQueryParameters3D, PhysicsServer3D, ShaderMaterial, SubViewport, Time, Viewport}, global::MouseButton, init::{gdextension, ExtensionLibrary}, meta::FromGodot, obj::{Base, Gd, WithBaseField}, prelude::{godot_api, GodotClass}};
 
 mod constants;
 use constants::*;
@@ -28,6 +30,8 @@ impl INode for GameRoot {
     }
 }
 
+// TODO: Rotation around current pos on plane, scrolling is distance from that point
+// TODO: Basically use pivot where pivot is clamped to bounds but not the actual cam
 #[derive(GodotClass)]
 #[class(base=Camera3D)]
 struct PanningCamera {
@@ -356,11 +360,16 @@ impl InputPassNode {
 struct FieldGripMap {
     base: Base<GridMap>,
     last_mouse_coords: Option<Vector3i>,
-    last_cell_type: i32,
+    last_highlight_cell_type: i32,
+    char_refs: HashMap<Vector3i, Gd<FieldCharacter>>,
+    char_focused: bool,
+    focus_highlighted_cells: Vec<Vector3i>,
 
     #[export] cam: Option<Gd<PanningCamera>>,
-    #[export] highlight_index: i32,
-    #[export] char_start_index: i32,
+    #[export] highlight_index: i32, // TODO: Move these to offsets
+    #[export] highlight_move_index: i32,
+    #[export] highlight_attack_index: i32,
+    #[export] highlight_heal_index: i32,
 }
 
 #[godot_api]
@@ -369,35 +378,83 @@ impl IGridMap for FieldGripMap {
         Self {
             base,
             last_mouse_coords: None,
-            last_cell_type: -1,
+            last_highlight_cell_type: -1,
+            char_refs: HashMap::new(),
+            char_focused: false,
+            focus_highlighted_cells: Vec::new(),
 
             cam: None,
             highlight_index: 0,
-            char_start_index: 0,
+            highlight_move_index: 0,
+            highlight_attack_index: 0,
+            highlight_heal_index: 0,
         }
     }
 
     fn unhandled_input(&mut self, event: Gd<InputEvent>) {
         if event.get_class() == "InputEventMouseButton".into() {
-            let _event: Gd<InputEventMouseButton> = event.cast(); // Cast won't fail due to above check
-        }
+            let event: Gd<InputEventMouseButton> = event.cast(); // Cast won't fail due to above check
+
+            if event.get_button_index() == MouseButton::LEFT && event.is_pressed() && !self.char_focused {
+                // Get currently moused over character
+                if let Some(mut pos) = self.last_mouse_coords {
+                    let mut move_range: u32 = 0;
+                    let mut attack_range: u32 = 0;
+                    let mut heal_range: u32 = 0;
+
+                    pos.y += 1; // Block above currently moused
+
+                    if let Some(char_ref) = self.char_refs.get(&pos) {
+                        // Since movement range doesn't include the current position, add 1
+                        move_range = char_ref.bind().get_movement_range() + 1;
+                        attack_range = char_ref.bind().get_attack_range();
+                        heal_range = char_ref.bind().get_heal_range();
+                        self.char_focused = true;
+                    }
+
+                    if move_range > 0 {
+                        self.show_char_ranges(pos, move_range, attack_range, heal_range);
+                    }
+                }
+            } else if event.get_button_index() == MouseButton::RIGHT && event.is_pressed() && self.char_focused {
+                // Exit out of being focused on a character
+                self.char_focused = false;
+                self.clear_char_ranges();
+            }
+        } /* else if event.get_class() == "InputEventMouseMovement".into() {
+            // Show range on hover
+            if let Some(mut pos) = self.last_mouse_coords {
+                if let Some(char_ref) = self.char_refs.get(&pos) {
+                    let move_range: u32 = char_ref.bind().get_movement_range();
+
+                    pos.y += 1; // Block above currently moused
+                    
+
+                    if move_range > 0 { self.show_char_ranges(pos, move_range); }
+
+                    self.char_focused = true;
+                } else {
+                    self.char_focused = false;
+                    self.clear_char_ranges();
+                }
+            }
+        } */
     }
 
     fn process(&mut self, _: f64) {
         // Mouse pos is calculated every frame for smoothness
         if let Some(cam) = self.get_cam() {
             if let Some(world_pos) = cam.bind().get_world_mouse_pos_option() {
-                let mut mouse_coords: Vector3i = self.get_coords_from_world_pos(world_pos);
-
-                // Adjust up one for block above intersecting
-                mouse_coords.y += 1;
+                let mouse_coords: Vector3i = self.get_coords_from_world_pos(world_pos);
 
                 if Some(mouse_coords) != self.last_mouse_coords {
+                    if !self.char_focused { self.last_highlight_cell_type = 0 } // Block can only be 0 if char not focused
+
                     if let Some(last_mouse_coords) = self.last_mouse_coords {
-                        self.set_overlay_block(last_mouse_coords, self.last_cell_type);
+                        self.set_overlay_block(last_mouse_coords, self.last_highlight_cell_type);
                     }
 
-                    self.last_cell_type = self.base().get_cell_item(mouse_coords);
+                    self.last_highlight_cell_type = self.base().get_cell_item(mouse_coords);
 
                     self.set_overlay_block(mouse_coords, self.highlight_index);
 
@@ -418,13 +475,175 @@ impl FieldGripMap {
 
     #[func]
     fn set_overlay_block(&mut self, overlay_coords: Vector3i, mesh_index: i32) {
+        // TODO: Change to offset here
         self.base_mut().set_cell_item(overlay_coords, mesh_index);
     }
 
     #[func]
-    fn clear_overlay_block(&mut self, overlay_coords: Vector3i) {
-        self.base_mut().set_cell_item(overlay_coords, -1);
+    fn map_to_local(&self, coords: Vector3i) -> Vector3 {
+        self.base().map_to_local(coords)
     }
+
+    // Change position of character currently on board
+    // Does not check whether char_ref at cur_pos is the same
+    // Overwrites whatever is at the position
+    #[func]
+    fn reposition_char(&mut self, char_ref: Gd<FieldCharacter>, cur_pos: Vector3i, new_pos: Vector3i) {
+        self.char_refs.remove_entry(&cur_pos);
+        self.char_refs.insert(new_pos, char_ref);
+    }
+
+    // Returns a map that includes all possible points the character can move
+    #[func]
+    fn get_range_map(&self, char_pos: Vector3i, mut range: u32, range_offset: u32) -> Vec<Vector3i> {
+        range = range + range_offset;
+
+        // TODO: Handle slopes, cliffs, etc
+        // TODO: Make enemies block as well
+        let probe_cells: Array<Vector3i> = self.base().get_used_cells();
+
+        // Filters probe cells to remove those that don't have space above them,
+        // and add one to y to return cells possible to move to
+        let probe_cells = probe_cells.iter_shared().filter_map(|mut pos| {
+            pos.y += 1; // Adj to cell above
+
+            if self.base().get_cell_item(pos) != GridMap::INVALID_CELL_ITEM {
+                None
+            } else {
+                Some(pos)
+            }
+        });
+
+        // Calculate range by just getting block diff between char and probe cell
+        // This means the range is how many blocks they can move, which is intended
+        let probe_cells = probe_cells.filter(|pos| {
+            let pos_diff: Vector3i = (char_pos - *pos).abs();
+            let distance: u32 = (pos_diff.x + pos_diff.y + pos_diff.z) as u32;
+
+            distance <= range && distance >= range_offset
+        });
+
+        // TODO: iterate over existing chars and remove them from being allowed for movement
+
+        probe_cells.collect()
+    }
+
+    #[func]
+    fn show_char_ranges(&mut self, char_pos: Vector3i, movement_range: u32, attack_range: u32, heal_range: u32) {
+        let mut healable: Vec<Vector3i> = self.get_range_map(char_pos, heal_range, movement_range);
+
+        for pos in healable.iter_mut() {
+            pos.y -= 1; // Highlights under the character
+            self.focus_highlighted_cells.push(*pos);
+
+            self.set_overlay_block(*pos, self.highlight_heal_index);
+        }
+
+        let mut attackable: Vec<Vector3i> = self.get_range_map(char_pos, attack_range, movement_range);
+
+        for pos in attackable.iter_mut() {
+            pos.y -= 1; // Highlights under the character
+            self.focus_highlighted_cells.push(*pos);
+
+            self.set_overlay_block(*pos, self.highlight_attack_index);
+        }
+
+        let mut reachable: Vec<Vector3i> = self.get_range_map(char_pos, movement_range, 0);
+
+        for pos in reachable.iter_mut() {
+            pos.y -= 1; // Highlights under the character
+            self.focus_highlighted_cells.push(*pos);
+
+            self.set_overlay_block(*pos, self.highlight_move_index);
+        }
+
+        
+
+        // Keep mouse highlight on field
+        if let Some(mouse_coords) = self.last_mouse_coords {
+            self.last_highlight_cell_type = self.highlight_move_index; // Draw movement range under mouse
+            self.set_overlay_block(mouse_coords, self.highlight_index);
+        }
+    }
+
+    #[func]
+    fn clear_char_ranges(&mut self) {
+        let last_range_cell_types: Vec<Vector3i> = self.focus_highlighted_cells.clone();
+
+        for pos in last_range_cell_types.into_iter() {
+            let mut mesh_index: i32 = 0;
+
+            // Don't clear if block is highlighted currently
+            if let Some(last_mouse_coords) = self.last_mouse_coords {
+                if pos == last_mouse_coords {
+                    self.last_highlight_cell_type = mesh_index;
+                    mesh_index = self.highlight_index;
+                }
+            }
+
+            self.set_overlay_block(pos, mesh_index);
+        }
+
+        self.focus_highlighted_cells.clear();
+    }
+
+    // TODO: Make this show/hide char information
+    // TODO: Do that by making char_focused Option<Gd<FieldCharacter>>
+    fn _set_char_focused(&mut self, val: bool) {
+        self.char_focused = val;
+    }
+}
+
+#[derive(GodotClass)]
+#[class(base=CharacterBody3D)]
+struct FieldCharacter {
+    base: Base<CharacterBody3D>,
+
+    #[export] #[var(get, set=set_field_pos)] field_position: Vector3i,
+    #[export] is_enemy: bool, // TODO: Replace with enum since there can be allies that you can't control
+    #[export] movement_range: u32,
+    #[export] attack_range: u32,
+    #[export] heal_range: u32,
+}
+
+#[godot_api]
+impl ICharacterBody3D for FieldCharacter {
+    fn init(base: Base<CharacterBody3D>) -> Self {
+        Self {
+            base,
+
+            field_position: Vector3i::ZERO,
+            is_enemy: false,
+            movement_range: 1,
+            attack_range: 1,
+            heal_range: 0,
+        }
+    }
+
+    fn ready(&mut self) {
+        self.set_field_pos(self.field_position);
+    }
+}
+
+#[godot_api]
+impl FieldCharacter {
+    #[func]
+    fn set_field_pos(&mut self, pos: Vector3i) {
+        if Engine::singleton().is_editor_hint() || self.base().is_inside_tree() {
+            // Update position based on FieldGridMap's coordinates
+            let mut field: Gd<FieldGripMap> = self.base().get_parent().expect("FieldCharacter should be direct child of a FieldMap")
+                .try_cast().expect("FieldCharacter should be direct child of a FieldMap");
+
+            self.base_mut().set_position(field.bind().map_to_local(pos));
+
+            // Update position on field
+            field.bind_mut().reposition_char(Gd::from_instance_id(self.base().instance_id()), self.field_position, pos);
+        }
+
+        self.field_position = pos;
+    }
+
+
 }
 
 // ColorRect that passes time to its shader material for more advanced shading
